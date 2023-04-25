9c9919ee43dbede101d68883a59c82379e17df63
Nicholas Carmack <dragecarmack@gmail.com>
2023-04-25 11:15:25 -0600

    completed form and all mvp

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,24 +1,46 @@
 import React from 'react'
+import { useState } from 'react'
 import { connect } from 'react-redux'
 import * as actionCreators from '../state/action-creators'
+import { resetForm } from '../state/action-creators'
 
 export function Form(props) {
 
-  const onChange = evt => {
+  const emptyFormState = {
+    newQuestion: '',
+    newTrueAnswer: '',
+    newFalseAnswer: ''
+  }
 
+  const initialFormState = {
+    newQuestion: props.form.newQuestion ? localStorage.getItem('newQuestion'):'',
+    newTrueAnswer: props.form.newTrueAnswer ? localStorage.getItem('newTrueAnswer'):'',
+    newFalseAnswer: props.form.newTrueAnswer ? localStorage.getItem('newFalseAnswer'):''
+  }
+
+  const [formState, setFormState] = useState(initialFormState)
+
+  const onChange = evt => {
+    setFormState({...formState, [evt.target.id] : evt.target.value})
+    localStorage.setItem([evt.target.id], evt.target.value)
+    props.inputChange(formState)
   }
 
   const onSubmit = evt => {
-
+    evt.preventDefault()
+    props.postQuiz({ "question_text": formState.newQuestion, "true_answer_text": formState.newTrueAnswer, "false_answer_text" : formState.newFalseAnswer})
+    localStorage.clear()
+    setFormState(emptyFormState)
   }
+
 
   return (
     <form id="form" onSubmit={onSubmit}>
       <h2>Create New Quiz</h2>
-      <input maxLength={50} onChange={onChange} id="newQuestion" placeholder="Enter question" />
-      <input maxLength={50} onChange={onChange} id="newTrueAnswer" placeholder="Enter true answer" />
-      <input maxLength={50} onChange={onChange} id="newFalseAnswer" placeholder="Enter false answer" />
-      <button id="submitNewQuizBtn">Submit new quiz</button>
+      <input maxLength={50} onChange={onChange} id="newQuestion" placeholder="Enter question" value={formState.newQuestion}/>
+      <input maxLength={50} onChange={onChange} id="newTrueAnswer" placeholder="Enter true answer" value={formState.newTrueAnswer}/>
+      <input maxLength={50} onChange={onChange} id="newFalseAnswer" placeholder="Enter false answer" value={formState.newFalseAnswer}/>
+      <button id="submitNewQuizBtn" disabled={!(formState.newQuestion.trim().length >1 && formState.newTrueAnswer.trim().length >1 && formState.newFalseAnswer.trim().length >1)}>Submit new quiz</button>
     </form>
   )
 }
